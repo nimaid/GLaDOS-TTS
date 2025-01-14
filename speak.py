dc2244da21fb64d638d17f49bb2e09909b435134
Ella Jameson <ellagjameson@gmail.com>
2025-01-14 13:28:44 -0700

    Removed checker

--- speak.py
+++ speak.py
@@ -1,11 +1,7 @@
 import argparse
 import sys
-from typing import Optional
-
-import time
 
 import glados
-from llamacheck import LlamaChecker
 
 # Parse arguments
 def parse_args(args):
@@ -19,36 +15,18 @@ def parse_args(args):
     parser.add_argument("-t", "--text", dest="text", type=str, required=True,
                         help="the text to speak aloud"
                         )
-    
-    parser.add_argument("-c", "--correct", dest="correct", action='store_true',
-                        help="if the text should be run through a Llama3.2-based spellchecker"
-                        )
 
     parsed_args = parser.parse_args(args)
 
     return parsed_args
 
-class TTS:
-    def __init__(self):
-        self.tts = glados.TTS()
-        self.lc = LlamaChecker()
-
-    def speak(self, text: str, correct: Optional[bool] = False):
-        if correct:
-            text = self.lc.correct_text(text)
-            print(f"GLaDOS (text corrected): \"{text}\"")
-        else:
-            print(f"GLaDOS: \"{text}\"")
-        
-        self.tts.speak_text_aloud(text)
-
 
 def main(args):
     parsed_args = parse_args(args)
     
-    tts = TTS()
+    tts = glados.TTS()
     
-    tts.speak(parsed_args.text, correct=parsed_args.correct)
+    tts.speak_text_aloud(parsed_args.text)
 
 
 def run():
