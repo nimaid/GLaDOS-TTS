8f085f3535a64746ee0f6fe8d275d398b498ca49
Ella Jameson <ellagjameson@gmail.com>
2025-01-14 19:39:07 -0700

    Made speech console a tkinter GUI

--- speak_console.py
+++ speak_console.py
@@ -1,14 +1,20 @@
 import sys
-from typing import Optional
+from typing import Optional, Callable
 import threading
 import time
+import tkinter as tk
 
 import glados
 
 
 class MessageQueue:
-    def __init__(self, delay: Optional[float] = 1.0):
+    def __init__(
+        self,
+        delay: Optional[float] = 1.0,
+        print_func: Optional[Callable[[str], None]] = print
+    ):
         self.delay = delay
+        self.print_func = print_func
         
         self.message_queue = []
         
@@ -23,7 +29,7 @@ class MessageQueue:
         if len(message) == 0:
             return
         
-        self._print(f"Adding message: \"{message}\"")
+        self.print_func(f"Adding message: \"{message}\"")
         
         self.message_queue.append(message)
         
@@ -46,7 +52,7 @@ class MessageQueue:
         message = self.message_queue[0]
         self.message_queue = self.message_queue[1:]
         
-        self._print(f"GLaDOS: \"{message}\"")
+        self.print_func(f"Reading message: \"{message}\"")
         self.tts.speak_text_aloud(message)
     
     def _message_loop(self):
@@ -55,17 +61,58 @@ class MessageQueue:
             self._process_message()
             time.sleep(self.delay)
         self.running = False
+
+
+class MainWindow:
+    def __init__(self):
+        self.w = tk.Tk()
+        self.w.title("GLaDOS TTS Engine")
+        self.w.resizable(width=False, height=False)
+        
+        self.text_box = tk.Text(self.w, state="disabled")
+        self.text_box.grid(row=0, column=0, columnspan=2, sticky="nsew")
+        
+        self.entry_box = tk.Entry(self.w)
+        self.entry_box.grid(row=1, column=0, sticky="nesw")
+        
+        self.submit_button = tk.Button(
+            self.w,
+            text = "Submit",
+            command = self._submit_button_func
+        )
+        self.submit_button.grid(row=1, column=1, sticky="nsew")
+        
+        self.w.bind("<Return>", lambda event: self._submit_button_func())
+        
+        self.w.columnconfigure(0, weight=20)
+        self.w.columnconfigure(1, weight=1)
+        
+        self.entry_box.focus_set()
+        
+        self.mq = MessageQueue(print_func = self._print_to_box)
+        
+        self.w.mainloop()
     
-    def _print(self, text: str):
-        print(text)
+    def _add_message(self, message: str):
+        self.mq.add(message)
+    
+    def _submit_button_func(self):
+        message = self.entry_box.get()
+        self._add_message(message)
+        
+        self.entry_box.delete(0, "end")
+        self.entry_box.focus_set()
+    
+    def _print_to_box(self, text: str):
+        self.text_box.configure(state="normal")
+        self.text_box.insert("end", f"{text}\n")
+        self.text_box.see("end")
+        self.text_box.configure(state="disabled")
 
 
 def main():
-    mq = MessageQueue()
+    mw = MainWindow()
     
-    while True:
-        message = input(">> ")
-        mq.add(message)
 
 main()
 
